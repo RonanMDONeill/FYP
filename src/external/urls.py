ee68a98829d1eba4e63d4b94ae62d74dda5b167d
RonanMDONeill <ronan.oneill@ucdconnect.ie>
2020-05-17 22:17:08 +0100

    Add files via upload

--- src/external/urls.py
+++ src/external/urls.py
@@ -3,6 +3,7 @@ from .views import (
 	scholar_view
 )
 
+# Define the URL paths for the External app
 urlpatterns = [
     path('<str:nodeType>/<str:nodeLabel>', scholar_view, name='external')
 ]
--- src/external/views.py
+++ src/external/views.py
@@ -1,28 +1,36 @@
 from django.shortcuts import render
 import scholarly
 
-# Create your views here.
+# Define the views for the External app
+
 def scholar_view(request, nodeType, nodeLabel):
+	# Call scholarly library to retrieve Google Scholar information
+
+	mostCited = None
+
+	# If request is for an Author
 	if nodeType == "Author":
 		try:
 			query = scholarly.search_author(nodeLabel)
 			result = next(query).fill()
 			mostCited = result.publications[0].fill()
 
+		# If Author cannot be found
 		except Exception as e:
 			result = "Sorry, we could not find this author's profile."
 			nodeType = "Bad query"
 			mostCited = None
 
+	# If request is for a Publicaiton
 	elif nodeType == "Publication":
 		try:
 			query = scholarly.search_pubs_query(nodeLabel)
 			result = next(query)
-			mostCited = None
 			print(result)
+
+		# If Publication cannot be found
 		except Exception as e:
 			result = "Sorry, we could not find this paper's profile."
 			nodeType = "Bad query"
-			mostCited = None
 
 	return render(request, "external/google_search_results.html", {"result": result, "nodeType": nodeType, "mostCited": mostCited})
